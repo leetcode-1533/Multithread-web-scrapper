556d0fa5302fffeb4578658629534e142cf430aa
y1275963 <1275963@gmail.com>
2014-08-08 18:15:04 -0700

    Two more bugs about the scraper working on it

--- testing_map.py
+++ testing_map.py
@@ -7,7 +7,31 @@ Created on Fri Aug  8 16:11:30 2014
 
 import MySQLdb as mdb
 import pandas.io.sql as psql
+import json
+import requests
+import folium
 
 con = mdb.connect('rosencrantz.berkeley.edu','kivalend','kivalend','kivalend')
 df_mysql = psql.frame_query('select * from country_link',con = con)
 
+locations = json.load(open('indiegogo/indiegogo_geo.json'))
+
+def get_locations(loc1):
+    if loc1 in locations:
+        return locations[loc1]
+    else:
+        location_list = [loc1]
+        postdata = {'locations':location_list}
+        response = requests.post('http://rosencrantz.berkeley.edu:5555/geo',data=postdata)
+        json_file =  json.loads(response.content)
+        loc_lat_long = json_file['results'][0]
+        return {'latitude':loc_lat_long['latitude'],'longitude':loc_lat_long['longitude']}
+mymap = folium.Map(location=[0,0],zoom_start=2)
+loc = df_mysql['name']
+counts = loc.value_counts()
+for test in loc.values:
+    coordinates = get_locations(test)
+    lat = coordinates['latitude']
+    lon = coordinates['longitude']
+    para = counts[test] * 50
+    mymap.circle_marker(location=[lat,lon],radius = para,fill_color = '#f00')
